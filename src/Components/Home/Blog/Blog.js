a210e425b4e023b60c22270e17db2550f6188685
MdRokybul <mdrokybulhasan40@gmail.com>
2020-10-29 22:42:09 +0600

    a style done

--- src/Components/Home/Blog/Blog.js
+++ src/Components/Home/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import './Blog.css';
 
 const Blog = () => {
     return (
-        <Container style={{ marginTop: '150px'}}>
+        <Container style={{ marginTop: '150px'}} className="blog-container">
             <h2 className="text-center">Blog</h2>
             <h2 className="text-center">Coming Soon...</h2>
         </Container>
